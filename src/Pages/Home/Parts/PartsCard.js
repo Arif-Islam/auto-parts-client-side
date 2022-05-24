133b2faed338f0e124f94b1e3b33bed3f4384d71
Arif <iariful950@gmail.com>
2022-05-24 20:10:22 +0600

    altered some width text size

--- src/Pages/Home/Parts/PartsCard.js
+++ src/Pages/Home/Parts/PartsCard.js
@@ -12,15 +12,15 @@ const PartsCard = ({ part, refetch }) => {
         <div className='relative w-[350px] md:w-[415px] shadow-lg hover:shadow-xl'>
             <div className='flex flex-col items-center justify-center rounded border'>
                 <div>
-                    <img className='w-64 hover:scale-90 transition ease-in-out md:w-72 object-cover rounded pb-2 hover:opacity-80 hover:cursor-text' src={image} alt="" />
+                    <img className='w-56 hover:scale-90 transition ease-in-out lg:w-72 object-cover rounded pb-2 hover:opacity-80 hover:cursor-text' src={image} alt="" />
                     <div className='absolute top-0 left-0'>
-                        <div className='bg-[#0eadc9] text-white text-lg font-medium p-3 rounded w-20 text-center'>
+                        <div className='bg-[#0eadc9] text-white lg:text-lg font-medium p-3 rounded w-20 text-center'>
                             <p>{price}</p>
                         </div>
                     </div>
                 </div>
                 <div className='h-[350px] md:h-[330px] p-4 '>
-                    <p className='bg-black text-white hover:text-gray-200 text-xl font-medium text-center p-2 rounded-sm mb-2'>{name}</p>
+                    <p className='bg-black text-white hover:text-gray-200 lg:text-xl font-medium text-center p-2 rounded-sm mb-2'>{name}</p>
                     <p className=' text-base tracking-tight text-justify mb-2 h-[120px] md:h-[100px] flex items-center overflow-hidden'>{description}</p>
                     <hr className='mb-2' />
                     <p className='text-center font-medium mb-1'>Minimum order quantity: {orderQuantity}</p>
--- src/Pages/Home/Reviews/ReviewCard.js
+++ src/Pages/Home/Reviews/ReviewCard.js
@@ -12,10 +12,10 @@ const ReviewCard = ({ singleReview, refetch }) => {
         <div className='shadow-lg rounded p-6 bg-white hover:shadow-xl'>
             <div className=' md:w-[500px]'>
                 <div className='flex space-x-4 items-center mb-4'>
-                    <img className='w-24 h-24 hover:scale-90 transition ease-in-out object-cover rounded-full border-cyan-500 border-2' src={image} alt="" />
+                    <img className='w-20 h-20 lg:w-24 lg:h-24 hover:scale-90 transition ease-in-out object-cover rounded-full border-cyan-500 border-2' src={image} alt="" />
                     <div className='flex flex-col'>
-                        <p className='text-lg text-[#0eadc9] font-semibold'>{name}</p>
-                        <div className='flex justify-center text-stone-200 w-full pb-2'>
+                        <p className='md:text-lg text-[#0eadc9] font-semibold'>{name}</p>
+                        <div className='flex  text-stone-200 w-full pb-2'>
                             <Rating
                                 initialRating={rating}
                                 emptySymbol={<FontAwesomeIcon icon={faStar} />}
